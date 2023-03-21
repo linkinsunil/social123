f5635f9478574ff6e6b98225806908c0c71643f9
Sunil Vishwakarma <sunilnet4@gmail.com>
2023-03-21 15:54:06 +0530

    Added route for Post Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Feed from './pages/Feed';
 import Login from './pages/Login';
+import Post from './pages/Post';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Feed />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/post/:slug' element={<Post />} />
         </Routes>
       </div>
     </BrowserRouter>
