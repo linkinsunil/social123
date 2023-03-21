9e6399018bc5659f86df152eb5f1b856c14cb6d3
Sunil Vishwakarma <sunilnet4@gmail.com>
2023-03-21 23:17:29 +0530

    added new comment and delete comment in Post Page

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -6,23 +6,70 @@ import LikeIcon from '../../assets/Like';
 import RepostIcon from '../../assets/Repost';
 import ShareIcon from '../../assets/Share';
 import Loader from '../../components/Loader';
+import AddComment from '../../assets/AddComment';
 import './Post.css';
+import Delete from '../../assets/Delete';
 
 const Post = () => {
   let { slug } = useParams();
   const [feedData, setFeedData] = useState('');
+  const [comment, setComment] = useState('');
+  const [comments, setComments] = useState([]);
 
+  // FETCH FEED DATA FROM API
   useEffect(() => {
     const getFeedData = async () => {
       const res = await axios.get(
         `https://api.realworld.io/api/articles/${slug}`
       );
-
       setFeedData(res.data.article);
     };
-
     getFeedData();
   }, []);
+
+  // POST NEW COMMENT
+  const postComments = async () => {
+    const token = localStorage.getItem('token');
+    const url = `https://api.realworld.io/api/articles/${slug}/comments`;
+    const data = {
+      comment: {
+        body: comment,
+      },
+    };
+    const config = {
+      headers: {
+        authorization: `Token ${token}`,
+      },
+    };
+
+    try {
+      const res = await axios.post(url, data, config);
+      console.log('RES-DATA COMMENT', res.data);
+      setComments(prev => [...prev, res.data.comment]);
+      setComment('');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // DELETE A COMMENT
+  const deleteComment = async comment => {
+    const token = localStorage.getItem('token');
+    const url = `https://api.realworld.io/api/articles/${slug}/comments/${comment.id}`;
+
+    const config = {
+      headers: {
+        authorization: `Token ${token}`,
+      },
+    };
+
+    try {
+      await axios.delete(url, config);
+      setComments(prev => prev.filter(item => item.id !== comment.id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className='card-container'>
@@ -81,7 +128,31 @@ const Post = () => {
               src={feedData.author.image}
               alt={feedData.author.username}
             />
-            <input type='text' placeholder='Comment on this...' />
+            <input
+              type='text'
+              placeholder='Comment on this...'
+              name='comment'
+              value={comment}
+              onChange={e => setComment(e.target.value)}
+            />
+            <div className='btn-comment' onClick={postComments}>
+              <AddComment />
+            </div>
+          </div>
+          <div className='comments-container'>
+            {comments.reverse().map(comment => (
+              <div key={comment.id} className='comments'>
+                <img
+                  className='avatar'
+                  src={comment.author.image}
+                  alt={comment.author.username}
+                />
+                <p>{comment.body}</p>
+                <div className='delete' onClick={() => deleteComment(comment)}>
+                  <Delete />
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       ) : (
