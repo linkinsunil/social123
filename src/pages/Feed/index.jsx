2a9bb4f97e199d67ba01143bfac7f38725df0096
Sunil Vishwakarma <sunilnet4@gmail.com>
2023-03-22 22:43:20 +0530

    added infinite-scroll library on feed page

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -3,32 +3,57 @@ import React, { useEffect, useState } from 'react';
 import FeedCard from '../../components/FeedCard';
 import Loader from '../../components/Loader';
 import './Feed.css';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Feed = () => {
   const [feedData, setFeedData] = useState('');
+  const [pageNumber, setPageNumber] = useState(0);
+  let url = `https://api.realworld.io/api/articles?limit=10&offset=${pageNumber}`;
+
   useEffect(() => {
-    const getFeedData = async () => {
-      const res = await axios.get(
-        'https://api.realworld.io/api/articles?limit=10&offset=0'
-      );
+    try {
+      const getFeedData = async () => {
+        const res = await axios.get(url);
+        setFeedData(res.data.articles);
+        setPageNumber(pageNumber + 1);
+      };
 
-      setFeedData(res.data.articles);
-    };
-
-    getFeedData();
+      getFeedData();
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
+  const fetchMoreData = async () => {
+    try {
+      setPageNumber(pageNumber + 1);
+      const res = await axios.get(url);
+      setFeedData(feedData.concat(res.data.articles));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      {feedData ? (
-        feedData.map(feed => (
-          <div key={feed.slug}>
-            <FeedCard feed={feed} />
-          </div>
-        ))
-      ) : (
-        <Loader />
-      )}
+    <div className='feed-container'>
+      <InfiniteScroll
+        dataLength={feedData.length}
+        next={fetchMoreData}
+        hasMore={feedData.length !== feedData.articlesCount}
+        loader={<Loader />}
+      >
+        <div className='feed-container'>
+          {feedData ? (
+            feedData.map((feed, index) => (
+              <div key={index}>
+                <FeedCard feed={feed} />
+              </div>
+            ))
+          ) : (
+            <div className='feed-container'></div>
+          )}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
